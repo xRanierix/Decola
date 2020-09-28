7fe137281d0f1877bdbd601874b81b496ada1b43
Ranieri <57837805+xRanierix@users.noreply.github.com>
2020-09-27 22:17:46 -0300

    Upgrade para Angular

---
+++ src/app/listacursos/listacursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listacursos',
+  templateUrl: './listacursos.component.html',
+  styleUrls: ['./listacursos.component.css']
+})
+export class ListacursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
